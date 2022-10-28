6c4281f3d2059dbae943752a6f9b5c8a894a31c0
azizdevfull <aziz16110904@gmail.com>
2022-10-28 15:32:36 +0500

    add product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -36,4 +37,42 @@ class AdminController extends Controller
         return redirect()->back()->with('message', 'Category Deleted Successfully');
 
     } 
+
+    public function view_product()
+    {
+        $category = Category::all();
+        return view('admin.product', compact('category'));
+    }
+    public function add_product(Request $request)
+    {
+        
+        $product = new Product;
+
+        $product->title=$request->title;
+
+        $product->description=$request->description;
+
+        $product->price=$request->price;
+
+        $product->quantity=$request->quantity;
+
+        $product->discount_price=$request->dis_price;
+
+        $product->category=$request->category;
+
+
+        $image = $request->image;
+
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+
+        $request->image->move('product',$imagename);
+
+        $product->image = $imagename;
+
+        
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product Added Successfully.');
+
+    }
 }
