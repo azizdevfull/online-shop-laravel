4ea9ae263780bf08c081aabd12acda7729b1e384
azizdevfull <aziz16110904@gmail.com>
2022-10-28 11:46:22 +0500

    my first laravel online shop project

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function view_category()
+    {
+    
+        $data = Category::all();
+
+        return view('admin.category',compact('data'));
+    }
+    
+    public function add_category(Request $request){
+        
+        $data = new category;
+
+        $data->category_name=$request->category;
+
+        $data->save();
+
+        return redirect()->back()->with('message', 'Category Added Successfully');
+
+    }
+    public function delete_category($id)
+    {
+
+        $data = Category::find($id);
+
+        $data->delete();
+
+        return redirect()->back()->with('message', 'Category Deleted Successfully');
+
+    } 
+}
