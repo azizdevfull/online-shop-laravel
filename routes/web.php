6ebbbdb96b4b95b9c4574bbbe8effdef9cb9984e
azizdevfull <aziz16110904@gmail.com>
2022-11-01 14:39:34 +0500

    added Email Verification

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,13 @@ use App\Http\Controllers\AdminController;
 route::get('/', [HomeController::class, 'index']);
 
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
 
-route::get('/redirect', [HomeController::class, 'redirect']);
+route::get('/redirect', [HomeController::class, 'redirect'])->middleware('auth','verified');
 
 route::get('/view_category', [AdminController::class, 'view_category']);
 
