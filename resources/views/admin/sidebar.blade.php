bcd73f3a9a9aba8ef74be211c3f59a902ee94784
azizdevfull <aziz16110904@gmail.com>
2022-11-02 18:35:48 +0500

    added dashboard link to dashboard tag

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -12,8 +12,8 @@
               <span class="count bg-success"></span>
             </div>
             <div class="profile-name">
-              <h5 class="mb-0 font-weight-normal">Henry Klein</h5>
-              <span>Gold Member</span>
+              <h5 class="mb-0 font-weight-normal">{{ Auth()->user()->name }}</h5>
+              <span>{{ Auth()->user()->email }}</span>
             </div>
           </div>
           <a href="#" id="profile-dropdown" data-toggle="dropdown"><i class="mdi mdi-dots-vertical"></i></a>
@@ -57,7 +57,7 @@
         <span class="nav-link">Navigation</span>
       </li>
       <li class="nav-item menu-items">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="{{ url('/redirect') }}">
           <span class="menu-icon">
             <i class="mdi mdi-speedometer"></i>
           </span>
