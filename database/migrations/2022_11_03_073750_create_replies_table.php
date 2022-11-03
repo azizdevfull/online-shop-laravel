11043fea1cdfc1f263301ec5533239a7954fff74
azizdevfull <aziz16110904@gmail.com>
2022-11-03 13:52:22 +0500

    Complete Comment & Reply System

--- database/migrations/2022_11_03_073750_create_replies_table.php
+++ database/migrations/2022_11_03_073750_create_replies_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('comment_id')->nullable();
+            $table->string('reply')->nullable();
+            $table->string('user_id')->nullable();
             $table->timestamps();
         });
     }
