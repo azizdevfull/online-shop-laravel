a77fac306c912f572fcaa956499f8de2232334e6
azizdevfull <aziz16110904@gmail.com>
2022-11-04 13:04:54 +0500

    Last work Fixing some Issues

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -17,6 +17,18 @@
           </div>
 
        </div>
+
+       @if (session()->has('message'))
+              
+       <div class="alert alert-success">
+
+         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+
+         {{ session()->get('message') }}
+       </div>
+
+       @endif
+
        <div class="row">
          @foreach ($product as $products)
              
